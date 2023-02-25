342ce538a277a1c79afb8c64c2471d96f912a6be
Rishabh Goel <31466188+mrgoyell@users.noreply.github.com>
2023-02-25 14:31:27 +0530

    Initial Commit with /chat communicating with chat GPT

---
+++ chat_gpt.py
@@ -0,0 +1,43 @@
+import os
+import openai
+
+secret = "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"
+open_ai_key = "open_ai_key"
+os.environ[open_ai_key] = secret
+openai.api_key = os.environ[open_ai_key]
+
+
+def get_response(prompt, model="text-davinci-003", max_tokens=150):
+    """
+
+    """
+    # response = openai.Completion.create(
+    #     model="text-davinci-003",
+    #     prompt="You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
+    #     temperature=0.5,
+    #     max_tokens=60,
+    #     top_p=1.0,
+    #     frequency_penalty=0.5,
+    #     presence_penalty=0.0,
+    #     stop=["You:"]
+    # )
+
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=max_tokens,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+
+    message = response.choices[0]['text']
+    # print(response.choices[0]['text'])
+    return message
+
+#
+# while True:
+#     ques = input()
+#     print(get_response(ques))
---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+from chat_gpt import get_response
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+class Message(BaseModel):
+    message: str
+
+
+@app.post("/chat")
+async def chat(query: Message):
+    # print(query)
+    message = query.message
+    query.message = get_response(message)
+    return query
