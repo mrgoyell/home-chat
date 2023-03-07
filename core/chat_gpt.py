5f8381aa69657f13b885652c7effa66221b5c4a3
Rishabh Goel <31466188+mrgoyell@users.noreply.github.com>
2023-03-07 08:25:56 +0530

    Updated chat_gpt.py

--- core/chat_gpt.py
+++ core/chat_gpt.py
@@ -1,26 +1,35 @@
 import os
 import openai
+from openai.error import Timeout
 
-secret = os.environ.get("OPENAI_KEY", "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")
+secret = os.environ.get("OPENAI_KEY", "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")
 open_ai_key = "open_ai_key"
 os.environ[open_ai_key] = secret
 openai.api_key = os.environ[open_ai_key]
+default_timeout = 4  # seconds
 
 
 def get_response(prompt, model="text-davinci-003", max_tokens=150):
     """
 
     """
-    response = openai.Completion.create(
-        model=model,
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=max_tokens,
-        top_p=1.0,
-        frequency_penalty=0.5,
-        presence_penalty=0.0,
-        stop=["You:"]
-    )
+    try:
+        response = openai.Completion.create(
+            model=model,
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=max_tokens,
+            top_p=1.0,
+            frequency_penalty=0.5,
+            presence_penalty=0.0,
+            stop=["You:"],
+            timeout=default_timeout,
+            request_timeout=default_timeout
+        )
+        message = response.choices[0]['text']
+    except Timeout:
+        message = "took longer to think than google has patience for, please try again, maybe with a query " \
+                  "with a potentially faster/smaller answer? Or try our mobile app ;)"
 
     # response = openai.Completion.create(
     #     model=model,
@@ -33,7 +42,6 @@ def get_response(prompt, model="text-davinci-003", max_tokens=150):
     #     stop=[" Human:", " AI:"]
     # )
 
-    message = response.choices[0]['text']
     # print(response.choices[0]['text'])
     return message
 
